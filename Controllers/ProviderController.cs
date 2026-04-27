54c41d9fdccc11d5299a9c0d6a6dce637ab9de04
dinaboueloyoun <dinaaboeloyoun@gmail.com>
2026-04-28 00:28:56 +0300

    Done with Provider files

---
+++ Controllers/ProviderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeServices.Controllers
+{
+    public class ProviderController : Controller
+    {
+        // الصفحة الرئيسية لمقدم الخدمة
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        // صفحة الطلبات المتاحة
+        public IActionResult AvailableRequests()
+        {
+            return View();
+        }
+
+        // صفحة تاريخ العمليات
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
